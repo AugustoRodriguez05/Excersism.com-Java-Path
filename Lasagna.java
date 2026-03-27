6c253535c3b1cf911861bd7be33047d91e2d6b13
AugustoRodriguez05 <augustorodriguez127@gmail.com>
2026-03-26 21:30:32 -0300

    Refactor Lasagna class methods for reuse.

--- Lasagna.java
+++ Lasagna.java
@@ -2,25 +2,14 @@ public class Lasagna {
         public int expectedMinutesInOven(){
             return 40;
         }
-
         public int remainingMinutesInOven(int time){
-            return 40 - time;
+            return expectedMinutesInOven() - time;
         }
-
         public int preparationTimeInMinutes(int layer){
             return layer * 2;
         }
-
-        public int totalTimeInMinutes(int layers, int minutes){
-            return (layers * 2) + minutes;
+       public int totalTimeInMinutes(int layers, int minutes){
+            return preparationTimeInMinutes(layers) + minutes;
         }
-
-       public static void main(String[] args) {
-            Lasagna lasagna = new Lasagna();
-            System.out.println(lasagna.expectedMinutesInOven());
-            System.out.println(lasagna.remainingMinutesInOven(30));
-            System.out.println(lasagna.preparationTimeInMinutes(2));
-            System.out.println(lasagna.totalTimeInMinutes(3, 20));
-   }
 }
 
